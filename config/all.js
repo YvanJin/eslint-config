956b0657c83195cb06ef1e77a24d513c0991e4ea
yvanjin <jinyufeng2009@163.com>
2022-06-23 14:17:07 +0800

    feat: support react

---
+++ config/all.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    './vue.js',
+    './react.js',
+  ],
+}
---
+++ config/react.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    './typescript.js',
+  ],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/react-in-jsx-scope': off,
+  },
+}
