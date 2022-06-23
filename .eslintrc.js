80f09e39af76a2a55266fde3acda7c9da46ddcde
yvanjin <jinyufeng2009@163.com>
2022-06-23 14:44:05 +0800

    feat: differentiate version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: require.resolve('./config/vue.js'),
+  extends: require.resolve('./config/basic.js'),
 }
--- config/all.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: [
-    './vue.js',
-    './react.js',
-  ],
-}
