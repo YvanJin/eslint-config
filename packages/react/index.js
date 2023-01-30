2d9f77b18bfd096800e310e6c7dedf9571b6cc45
YvanJin <jinyufeng2009@163.com>
2023-01-30 08:11:56 +0800

    feat: add react-hooks

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,11 +1,14 @@
 module.exports = {
   extends: [
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     '@yvanjin/eslint-config-typescript',
   ],
   rules: {
     'jsx-quotes': ['error', 'prefer-double'],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
